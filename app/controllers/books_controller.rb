de238c4e94548ce432bf49eb861d2b470fc8c435
donyd <donal_dsilva@hotmail.com>
2019-04-01 15:00:33 +0100

    more book resource fixes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,64 @@ class BooksController < ApplicationController
   end
 
   def show
+    @reader = Reader.find(params[:reader_id])
+    @bookshelf = @reader.bookshelves.find(params[:bookshelf_id])
+    @book = @bookshelf.books.find(params[:id])
   end
 
   def new
+    # Associate a book with a bookshelf = id
+    # /bookshelf/1/book/new
+    @reader = Reader.find(params[:reader_id])
+    @bookshelf = @reader.bookshelves.find(params[:bookshelf_id])
+    @book = @bookshelf.books.build
+  end
+
+  def create
+    # Associate a book with a bookshelf = id
+    # /bookshelf/1/book
+    @reader = Reader.find(params[:reader_id])
+    @bookshelf = @reader.bookshelves.find(params[:bookshelf_id])
+    @book = @bookshelf.books.build(params.require(:book).permit(:title))
+
+    if @book.save
+      redirect_to reader_bookshelf_book_url(@reader, @bookshelf, @book)
+    else
+      render :action => "new"
+    end
   end
 
   def edit
+    # Edit book = id
+    # /reader/1/bookshelf/2/books/1/edit
+    @reader = Reader.find(params[:reader_id])
+    @bookshelf = @reader.bookshelves.find(params[:bookshelf_id])
+    @book = @bookshelf.books.find(params[:id])
   end
+
+  def update
+    @reader = Reader.find(params[:reader_id])
+    @bookshelf = @reader.bookshelves.find(params[:bookshelf_id])
+    @book = @bookshelf.books.find(params[:id])
+
+    if @book.update_attributes(params.require(:book).permit(:title))
+      redirect_to reader_bookshelf_book_url(@reader, @bookshelf, @book)
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @reader = Reader.find(params[:reader_id])
+    @bookshelf = @reader.bookshelves.find(params[:bookshelf_id])
+    @book = @bookshelf.books.find(params[:id])
+
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to reader_bookshelves_path(@reader) }
+      format.xml { head :ok }
+    end
+  end
+
+
 end
